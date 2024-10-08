0c5d7c5f357c48eb3323f1bebdfe8e6c33cbc6e7
aman8387 <119442432+aman8387@users.noreply.github.com>
2024-10-08 13:35:46 +0530

    Time: 1 ms (99.8%), Space: 46 MB (48.12%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+         int ans = 0;
+
+         for (int element : nums){
+            ans = ans^element;
+         }
+        return ans;
+    }
+}
