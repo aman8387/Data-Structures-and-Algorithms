a787a7fae24ff45a5e468bd7f0950108889ebd8e
aman8387 <119442432+aman8387@users.noreply.github.com>
2025-01-27 15:52:17 +0530

    Time: 1 ms (85.9%), Space: 44.5 MB (99.67%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -3,14 +3,10 @@ import java.util.List;
 
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        // Create a result list to store merged pairs
-        List<int[]> result = new ArrayList<>(); // Using a list for dynamic size
-
-        // Initialize two pointers
+        List<int[]> result = new ArrayList<>(); //list for dynamic size
         int i = 0;
         int j = 0;
 
-        // Traverse both arrays using two pointers
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i][0] < nums2[j][0]) {
                 result.add(nums1[i]);
@@ -19,19 +15,16 @@ class Solution {
                 result.add(nums2[j]);
                 j++;
             } else {
-                // IDs match: sum the values and add the pair to the result
                 result.add(new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]});
                 i++;
                 j++;
             }
         }
-        // Add remaining elements from nums1
-        while (i < nums1.length) {
+        while (i < nums1.length) {// Add remaining elements from nums1
             result.add(nums1[i]);
             i++;
         }
-        // Add remaining elements from nums2
-        while (j < nums2.length) {
+        while (j < nums2.length) {// Add remaining elements from nums2
             result.add(nums2[j]);
             j++;
         }
