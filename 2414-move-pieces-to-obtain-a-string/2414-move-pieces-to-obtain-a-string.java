939c931504527a44d7445158c17a76d55c98c311
aman8387 <119442432+aman8387@users.noreply.github.com>
2025-02-08 20:02:23 +0530

    Time: 15 ms (58.11%), Space: 46.2 MB (17.17%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -1,37 +1,28 @@
 class Solution {
     public boolean canChange(String start, String target) {
         int n = start.length();
+        int sid = 0, tid = 0;
 
-        String startFiltered = start.replace("_", "");
-        String targetFiltered = target.replace("_", "");
-        if (!startFiltered.equals(targetFiltered)) {
-            return false;
-        }
-
-        int sid= 0;
-        int tid = 0;
-
-        while (sid < n && tid< n){
-            while(sid<n && start.charAt(sid)=='_'){
+        while (sid < n || tid < n) {
+            while (sid < n && start.charAt(sid) == '_') {
                 sid++;
             }
-            while(tid<n && target.charAt(tid)=='_'){
+            while (tid < n && target.charAt(tid) == '_') {
                 tid++;
             }
-            if(sid == n && tid == n){
-                return true;
-            }
-            if(sid == n|| tid == n){
-                return false;
-            }
-            if(start.charAt(sid) != target.charAt(tid) || 
-            start.charAt(sid) == 'L' && sid<tid || 
-            start.charAt(sid) == 'R' && tid<sid){
-                return false;
-            }
+            if (sid == n && tid == n) return true;
+            if (sid == n || tid == n) return false;
+
+            if (start.charAt(sid) != target.charAt(tid)) return false;
+
+            if (start.charAt(sid) == 'L' && sid < tid) return false;
+
+            if (start.charAt(sid) == 'R' && sid > tid) return false;
+
             sid++;
             tid++;
         }
+
         return true;
     }
 }
